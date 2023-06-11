07a52ef027341cfa6f4102da545d39dcf69bc010
Swapnita Sahare <>
2023-06-11 23:04:06 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,63 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m) {
+  # Initialize the cache
+  cache <- NULL
+  
+  set <- function(matrix) {
+    m <<- matrix
+    cache$inv <<- NULL  # Invalidate the cached inverse
+  }
+  
+  get <- function() {
+    m
+  }
+  
+  setInverse <- function(inv) {
+    cache$inv <<- inv
+  }
+  
+  getInverse <- function() {
+    cache$inv
+  }
+  
+  # Create the cache object
+  cache <- list(set = set, get = get,
+                setInverse = setInverse,
+                getInverse = getInverse)
+  
+  # Set the initial matrix
+  set(m)
+  
+  # Return the cache object
+  cache
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cacheMat) {
+  # Check if the inverse is already cached
+  inv <- cacheMat$getInverse()
+  
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  # If not, compute the inverse
+  m <- cacheMat$get()
+  inv <- solve(m)
+  
+  # Cache the inverse
+  cacheMat$setInverse(inv)
+  
+  inv
 }
+m <- matrix(c(1, 2, 3, 4), nrow = 2)
+cacheMat <- makeCacheMatrix(m)
+inv <- cacheSolve(cacheMat)
+print(inv)
+cachedInv <- cacheSolve(cacheMat)
+print(cachedInv)
+
+
+
